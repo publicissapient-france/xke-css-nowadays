e9ce6aed491cbb986315b29af8dffeda7d430b7e
Alexis Tessier <alexistessierdev@gmail.com>
2018-03-15 13:53:55 +0100

    add router routes for sign-in and sign-up

--- step-3/src/App.js
+++ step-3/src/App.js
@@ -1,5 +1,15 @@
 import React from 'react';
 
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
+
 export default function App(){
-  return 'hello world'
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Redirect exact path='/' to='/sign-in'/>
+        <Route path='/sign-in' component={()=>"sign in"}/>
+        <Route path='/sign-up' component={()=>"sign up"}/>
+      </Switch>
+    </BrowserRouter>
+  )
 }
--- step-3/src/index.js
+++ step-3/src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ModernNormalize from 'react-modern-normalize';
 import App from './App';
-
-
 ReactDOM.render((
 	<ModernNormalize>
 		<App />
