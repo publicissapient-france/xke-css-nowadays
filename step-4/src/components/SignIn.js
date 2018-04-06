e5c714378ef8241c44751428fc2001736d6b1d60
Alexis Tessier <alexistessierdev@gmail.com>
2018-04-06 14:24:32 +0200

    use composes with css modules

--- step-4/src/components/SignIn.js
+++ step-4/src/components/SignIn.js
@@ -2,18 +2,25 @@ import React from 'react'
 
 import { Link } from 'react-router-dom'
 
+import {
+	container,
+	form,
+	form_input,
+	form_link
+} from '../index.css'
+
 import { form_submit_button } from './SignIn.css'
 
 export default function SignIn(){
 	return (
-		<div className="container">
-			<form className="form">
-				<input className="form_input" type="text" placeholder="Email" />
-				<input className="form_input" type="password" placeholder="password" />
+		<div className={container}>
+			<form className={form}>
+				<input className={form_input} type="text" placeholder="Email" />
+				<input className={form_input} type="password" placeholder="password" />
 
 				<button className={form_submit_button} type="submit">Log in</button>
-				<Link className="form_link" to="/">Forgot your password?</Link>
-				<Link className="form_link" to="/sign-up">Need an account? Sign up</Link>
+				<Link className={form_link} to="/">Forgot your password?</Link>
+				<Link className={form_link} to="/sign-up">Need an account? Sign up</Link>
 			</form>
 		</div>
 	)
--- step-4/src/components/SignUp.js
+++ step-4/src/components/SignUp.js
@@ -2,19 +2,26 @@ import React from 'react'
 
 import { Link } from 'react-router-dom'
 
+import {
+	container,
+	form,
+	form_input,
+	form_link
+} from '../index.css'
+
 import { form_submit_button } from './SignUp.css'
 
 export default function SignUp(){
 	return (
-		<div className="container">
-			<form className="form">
+		<div className={container}>
+			<form className={form}>
 				<h1>Create your account</h1>
 				<label>Enter your email address</label>
 
-				<input className="form_input" type="text" placeholder="Email" />
+				<input className={form_input} type="text" placeholder="Email" />
 
 				<button className={form_submit_button} type="submit">Continue</button>
-				<Link className="form_link" to="/sign-in">Already have an account? Log in</Link>
+				<Link className={form_link} to="/sign-in">Already have an account? Log in</Link>
 			</form>
 		</div>
 	)
